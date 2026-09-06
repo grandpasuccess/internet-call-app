557011b9f46c16b7d25a84cb1e1bc05635ad69fa
grandpasuccess <grandpasuccess@gmail.com>
2026-09-06 23:06:06 +0100

    fix(backend): Use default import for authenticate middleware in calls routes

--- backend/src/routes/calls.js
+++ backend/src/routes/calls.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { authenticate } = require('../middleware/authenticate');
+const authenticate = require('../middleware/authenticate');
 const callController = require('../controllers/callController');
 
 const router = express.Router();
